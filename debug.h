55433b8d7a17b3b96b06d95d8f63abb3c84117fb
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-09 15:12:54 +0300

    chunk construction, instruction writing and extraction

---
+++ debug.h
@@ -0,0 +1,13 @@
+//
+// Created by Patrick Kariuki on 1/9/25.
+//
+
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //CLOX_DEBUG_H
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#include "common.h"
 
-int main(void) {
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+
+int main(int argc, const char* argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
