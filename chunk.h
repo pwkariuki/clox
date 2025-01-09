9ce448d8adbf99f23d960d959ee43595cc93a281
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-09 19:32:07 +0300

    constant representations and instructions

--- chunk.h
+++ chunk.h
@@ -6,8 +6,10 @@
 #define CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -15,10 +17,12 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif //CHUNK_H
