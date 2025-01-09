56aba45dc32d4498e4efbc9210d383e4e51ba627
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-09 09:41:45 +0300

    initial clox setup

---
+++ common.h
@@ -0,0 +1,12 @@
+//
+// Created by Patrick Kariuki on 1/9/25.
+//
+
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CLOX_COMMON_H
