214c4af869712a3ec128c6ef72e0a4945e2e06f5
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-19 11:23:22 +0100

    booleans, nil values, and comparisons

--- value.c
+++ value.c
@@ -7,6 +7,23 @@
 #include "memory.h"
 #include "value.h"
 
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) {
+    return false;
+  }
+
+  switch (a.type) {
+    case VAL_BOOL:
+      return AS_BOOL(a) == AS_BOOL(b);
+    case VAL_NIL:
+      return true;
+    case VAL_NUMBER:
+      return AS_NUMBER(a) == AS_NUMBER(b);
+    default:
+      return false; // Unreachable.
+  }
+}
+
 void initValueArray(ValueArray* array) {
   array->capacity = 0;
   array->count = 0;
@@ -30,5 +47,15 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
-  printf("%g", AS_NUMBER(value));
+  switch (value.type) {
+    case VAL_BOOL:
+      printf(AS_BOOL(value) ? "true" : "false");
+      break;
+    case VAL_NIL:
+      printf("nil");
+      break;
+    case VAL_NUMBER:
+      printf("%g", AS_NUMBER(value));
+      break;
+  }
 }
