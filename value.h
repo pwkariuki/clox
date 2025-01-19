8d4bd17f4a1d015e15565737c042b1a947f6d724
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-19 17:22:45 +0100

    string initialization

--- value.h
+++ value.h
@@ -7,9 +7,13 @@
 
 #include "common.h"
 
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
 typedef enum {
   VAL_BOOL,
   VAL_NIL,
+  VAL_OBJ,
   VAL_NUMBER,
 } ValueType;
 
@@ -18,6 +22,7 @@ typedef struct {
   union {
     bool boolean;
     double number;
+    Obj* obj;
   } as;
 } Value;
 
@@ -25,6 +30,7 @@ typedef struct {
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NIL(value)     ((value).type == VAL_NIL)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
 // Unpack to get C value before doing anything.
 #define AS_BOOL(value)    ((value).as.boolean)
@@ -34,6 +40,7 @@ typedef struct {
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
 
 typedef struct {
   int capacity;
