91c8d0ecea4fd54364d4d1519e165d0e382dee56
Patrick Kariuki <pkariuk1@jhu.edu>
2025-01-19 19:52:44 +0100

    hash table implementation and optimizations

---
+++ table.h
@@ -0,0 +1,29 @@
+//
+// Created by Patrick Kariuki on 1/19/25.
+//
+
+#ifndef CLOX_TABLE_H
+#define CLOX_TABLE_H
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+  ObjString* key;
+  Value value;
+} Entry;
+
+typedef struct {
+  int count;
+  int capacity;
+  Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(const Table* from, Table* to);
+
+#endif //CLOX_TABLE_H
