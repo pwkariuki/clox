e4da9d3e10240a95f2437efadb76d37282122fa4
Patrick Kariuki <pkariuk1@jhu.edu>
2025-11-01 15:17:01 -0400

    executing super accesses

--- compiler.c
+++ compiler.c
@@ -543,6 +543,23 @@ static Token syntheticToken(const char* text) {
     return token;
 }
 
+// compile `super` access expression
+static void super_(bool canAssign) {
+    if (currentClass == NULL) {
+        error("Can't use 'super' outside of a class.");
+    } else if (!currentClass->hasSuperclass) {
+        error("Can't use 'super' in a class with no superclass.");
+    }
+
+    consume(TOKEN_DOT, "Expect '.' after 'super'.");
+    consume(TOKEN_IDENTIFIER, "Expect superclass method name.");
+    uint8_t name = identifierConstant(&parser.previous);
+    // look up the receiver `this` and superclass from its `super` variable
+    namedVariable(syntheticToken("this"), false);
+    namedVariable(syntheticToken("super"), false);
+    emitBytes(OP_GET_SUPER, name);
+}
+
 // compile `this` like a local variable
 // closures inside methods referencing this will capture
 // the receiver in an upvalue
@@ -604,7 +621,7 @@ ParseRule rules[] = {
   [TOKEN_OR]            = {NULL,     or_,    PREC_OR},
   [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
   [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SUPER]         = {super_,   NULL,   PREC_NONE},
   [TOKEN_THIS]          = {this_,    NULL,   PREC_NONE},
   [TOKEN_TRUE]          = {literal,  NULL,   PREC_NONE},
   [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
@@ -754,15 +771,16 @@ static void classDeclaration() {
         if (identifiersEqual(&className, &parser.previous)) {
             error("A class can't inherit from itself");
         }
+
+        // store superclass as local variable
+        beginScope();
+        addLocal(syntheticToken("super"));
+        defineVariable(0);
+
         namedVariable(className, false);
         emitByte(OP_INHERIT);
         classCompiler.hasSuperclass = true;
     }
-
-    // store superclass as local variable
-    beginScope();
-    addLocal(syntheticToken("super"));
-    defineVariable(0);
 
     namedVariable(className, false); // load class name onto the stack for method binding
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
