79d4adba865290774dda310674cb483046c1fe80
Patrick Kariuki <pkariuk1@jhu.edu>
2025-11-02 11:50:30 -0500

    clox complete

--- common.h
+++ common.h
@@ -10,13 +10,13 @@
 #include <stdint.h>
 
 #define NAN_BOXING
-// #define DEBUG_PRINT_CODE
-// #define DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
 
 // run gc as often as possible
-// #define DEBUG_STRESS_GC
+#define DEBUG_STRESS_GC
 // log gc
-// #define DEBUG_LOG_GC
+#define DEBUG_LOG_GC
 
 // limit the number of local variables you can have in scope
 #define UINT8_COUNT (UINT8_MAX + 1)
